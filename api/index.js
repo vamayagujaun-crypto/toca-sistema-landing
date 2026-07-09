20c624590e2332898f3870e3bb41958622a3294b
vamayagujaun-crypto <vamayagu.jaun@gmail.com>
2026-07-09 00:32:14 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 module.exports = (req, res) => {
+  // Estos son los datos de tu cliente (ejemplo)
+  const cliente = {
+    nombre: "Víctor Amaya",
+    empresa: "Toca Sistema",
+    cargo: "Fundador",
+    telefono: "+573000000000",
+    email: "contacto@tocasistema.com"
+  };
+
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Si ves esto, Toca Sistema esta vivo!</h1>');
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(cliente));
 };
